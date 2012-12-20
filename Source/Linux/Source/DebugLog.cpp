e3c896658d38ff9eaf56b8223b893bfa4ccc6c3e
Rico <teh_programerer@interactivelaboratories.com>
2012-12-20 21:07:02 +0000

    |SOURCE| [Linux::DebugLog] Implemented terminal and file log outputting

---
+++ Source/Linux/Source/DebugLog.cpp
@@ -0,0 +1,42 @@
+#include <Debugger/DebugLog.hpp>
+#include <cstdarg>
+#include <cstdio>
+
+namespace BD
+{
+	BD_UINT32 Trace( const char *p_pFileName, const char *p_pMessage, ... )
+	{
+		char CompleteMessage[ 1024 ];
+		BD_UINT32 ReturnVal;
+
+		va_list ArgPtr;
+
+		va_start( ArgPtr, p_pMessage );
+		ReturnVal = vsnprintf( CompleteMessage, 1024, p_pMessage, ArgPtr );
+		va_end( ArgPtr );
+
+		if( p_pFileName != BD_NULL )
+		{
+			FILE *pFileHandle = BD_NULL;
+
+			pFileHandle = fopen( p_pFileName, "w+" );
+
+			if( pFileHandle == BD_NULL )
+			{
+				return BD_ERROR;
+			}
+
+			fprintf( pFileHandle, "%s", CompleteMessage );
+
+			fclose( pFileHandle );
+			pFileHandle = BD_NULL;
+		}
+		else
+		{
+			printf( "%s", CompleteMessage );
+		}
+
+		return ReturnVal;
+	}
+}
+
--- Source/Linux/Source/Main.cpp
+++ Source/Linux/Source/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <DataTypes.hpp>
 #include <LinuxWindow.hpp>
+#include <Debugger/DebugLog.hpp>
 
 int main( int p_Argc, char **p_ppArgv )
 {
@@ -8,6 +9,7 @@ int main( int p_Argc, char **p_ppArgv )
 	Test.Create( 800, 600 );
 
 	sleep( 5 );
+	bdTrace( BD_NULL, "------------ TEST: %s -------------\n", "Linux" );
 	std::cout << "BrainDead" << std::endl;
 	return 0;
 }
